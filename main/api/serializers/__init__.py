5e31d9528f7c9c0c7bd6fbe232a83673ff8a1ac4
shagorrobidas <roysagor88@gmail.com>
2026-02-09 05:47:35 +0600

    serializer init

---
+++ main/api/serializers/__init__.py
@@ -0,0 +1,11 @@
+from .profile_serializer import ProfileSerializer
+from .services import ServiceSerializer
+from .testimonial import TestimonialSerializer
+from .contact import ContactMessageSerializer
+
+__all__ = [
+    'ProfileSerializer',
+    'ServiceSerializer',
+    'TestimonialSerializer',
+    'ContactMessageSerializer'
+]
