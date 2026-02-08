2bf8f252104157235f3c87c2baa63d4504a23cf5
shagorrobidas <roysagor88@gmail.com>
2026-02-09 05:46:30 +0600

    view init

---
+++ main/api/views/__init__.py
@@ -0,0 +1,11 @@
+from .profile import ProfileDetail
+from .services import ServiceViewSet
+from .testimonial import TestimonialViewSet
+from .contact import ContactMessageCreate
+
+__all__ = [
+    'ProfileDetail',
+    'ServiceViewSet',
+    'TestimonialViewSet',
+    'ContactMessageCreate'
+]
