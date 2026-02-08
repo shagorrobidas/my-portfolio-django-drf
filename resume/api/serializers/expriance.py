336c99c042849d5424177f529dfae8e32b2b66fa
shagorrobidas <roysagor88@gmail.com>
2026-02-09 05:34:08 +0600

    expriance serializer

---
+++ resume/api/serializers/expriance.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from resume.models import Experience
+
+
+class ExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Experience
+        fields = '__all__'
+        read_only_fields = ('id',)
