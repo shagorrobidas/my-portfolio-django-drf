1fd6134bdd31b10c11e162d68e8f786555f0fe0b
shagorrobidas <roysagor88@gmail.com>
2026-02-09 05:30:01 +0600

    add template

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
