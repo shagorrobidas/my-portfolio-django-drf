c018a9bc8b54ac534978a9401d55a3788080cf92
shagorrobidas <roysagor88@gmail.com>
2026-02-09 03:35:10 +0600

    Post admin create

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'date')
+    search_fields = ('title', 'category')
+    list_filter = ('category', 'date')
