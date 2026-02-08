e72c202de3235a807511fff6c15c1f135870af0e
shagorrobidas <roysagor88@gmail.com>
2026-02-09 05:42:40 +0600

    view init

---
+++ resume/api/views/__init__.py
@@ -0,0 +1,11 @@
+from .education import EducationSerializer
+from .expriance import ExperienceSerializer 
+from .skills import SkillSerializer
+
+__all__ = [
+    'EducationSerializer',
+    'ExperienceSerializer',
+    'SkillSerializer'
+]
+
+
