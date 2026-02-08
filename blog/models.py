c0edce41d298308fa15291f386b8c7eebaea41d9
shagorrobidas <roysagor88@gmail.com>
2026-02-09 03:34:37 +0600

    Post model create

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    date = models.DateField()
+    image = models.ImageField(upload_to='blog/', blank=True, null=True)
+    summary = models.TextField()
+    content = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
