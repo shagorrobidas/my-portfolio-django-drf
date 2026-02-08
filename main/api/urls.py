5519a6caa5dd20d24ddf8af3c7598ef23f182d99
shagorrobidas <roysagor88@gmail.com>
2026-02-09 05:47:07 +0600

    main api url

--- main/api/urls.py
+++ main/api/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from main.api.views import ProfileDetail
+from main.api.views import (
+    ProfileDetail,
+    ServiceViewSet,
+    TestimonialViewSet,
+    ContactMessageCreate
+)
+
+
 
 router = DefaultRouter()
-
+router.register(r'services', ServiceViewSet)
+router.register(r'testimonials', TestimonialViewSet)
 urlpatterns = [
     path(
         'profile/',
@@ -14,4 +22,9 @@ urlpatterns = [
         '',
         include(router.urls)
     ),
+    path(
+        'contact/',
+        ContactMessageCreate.as_view(),
+        name='contact'
+    )
 ]
