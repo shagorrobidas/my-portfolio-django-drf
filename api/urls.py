d0675ba807df5e23bc7cd2ac1b44370fa6b868ea
shagorrobidas <roysagor88@gmail.com>
2026-02-09 23:50:16 +0600

    fix urls

--- api/urls.py
+++ api/urls.py
@@ -1,25 +1,8 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path(
-        'main/',
-        include('main.api.urls'),
-        name='main'
-    ),
-    path(
-        'resume/',
-        include('resume.api.urls'),
-        name='resume'
-    ),
-    path(
-        'project/',
-        include('project.api.urls'),
-        name='project'
-    ),
-    path(
-        'blog/',
-        include('blog.api.urls'),
-        name='blog'
-    ),
-    
-]   
+    path('', include('main.api.urls')),
+    path('', include('resume.api.urls')),
+    path('', include('project.api.urls')),
+    path('', include('blog.api.urls')),
+]
