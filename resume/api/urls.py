82a19e0ec8976cd87dfd9737edf22da204938176
shagorrobidas <roysagor88@gmail.com>
2026-02-09 06:01:20 +0600

    urlsd

---
+++ resume/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ExperienceViewSet, EducationViewSet, SkillViewSet
+
+router = DefaultRouter()
+router.register(r'experience', ExperienceViewSet)
+router.register(r'education', EducationViewSet)
+router.register(r'skills', SkillViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
