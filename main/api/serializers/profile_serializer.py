72107631cb1ed43b3bcd3d93d802c00521eb552a
shagorrobidas <roysagor88@gmail.com>
2026-02-09 23:53:42 +0600

    profile serializer modified

--- main/api/serializers/profile_serializer.py
+++ main/api/serializers/profile_serializer.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
 from main.models import Profile
 
+
 class ProfileSerializer(serializers.ModelSerializer):
+    linkedin = serializers.CharField(source='linkedin_username', read_only=True)
+    github = serializers.CharField(source='github_username', read_only=True)
+
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = [
+            'id',
+            'name',
+            'title',
+            'avatar',
+            'email',
+            'linkedin',
+            'github',
+            'linkedin_username',
+            'linkedin_url',
+            'github_username',
+            'github_url',
+            'birthday',
+            'location',
+            'about_text',
+            'about_text_extra'
+        ]
         read_only_fields = ('id',)
