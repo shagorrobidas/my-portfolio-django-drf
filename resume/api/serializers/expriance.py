6172b92a16085c30f3ef166d15f6669f07c2b8e6
shagorrobidas <roysagor88@gmail.com>
2026-02-09 23:56:17 +0600

    add range

--- resume/api/serializers/expriance.py
+++ resume/api/serializers/expriance.py
@@ -3,7 +3,12 @@ from resume.models import Experience
 
 
 class ExperienceSerializer(serializers.ModelSerializer):
+    date_range = serializers.SerializerMethodField()
+
     class Meta:
         model = Experience
         fields = '__all__'
         read_only_fields = ('id',)
+
+    def get_date_range(self, obj):
+        return f"{obj.start_date} - {obj.end_date or 'Present'}"
