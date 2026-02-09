1831ca93e08e59e0f772e4cfed1914b5645c9417
shagorrobidas <roysagor88@gmail.com>
2026-02-10 05:45:51 +0600

    view init

--- main/api/views/__init__.py
+++ main/api/views/__init__.py
@@ -2,10 +2,14 @@ from .profile import ProfileDetail
 from .services import ServiceViewSet
 from .testimonial import TestimonialViewSet
 from .contact import ContactMessageCreate
+from .client import ClientViewSet
+from .social import SocialLinkViewSet
 
 __all__ = [
     'ProfileDetail',
     'ServiceViewSet',
     'TestimonialViewSet',
-    'ContactMessageCreate'
+    'ContactMessageCreate',
+    'ClientViewSet',
+    'SocialLinkViewSet'
 ]
