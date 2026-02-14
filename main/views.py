ac4dccfa4bf2b8fe437276c1ecd226edd69cb5cb
shagorrobidas <roysagor88@gmail.com>
2026-02-15 02:24:17 +0600

    home, protfolio, resume, blog,contact view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'about.html')
+
+
+def resume(request):
+    return render(request, 'resume.html')
+
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
