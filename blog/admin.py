735fce1230eaa7642d6230e2e33863720d0d11bc
shagorrobidas <roysagor88@gmail.com>
2026-02-15 06:17:55 +0600

    fix bug

--- blog/admin.py
+++ blog/admin.py
@@ -3,13 +3,18 @@ from blog.models import Post, Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
+    list_display = ('title', 'get_categories', 'date')
     search_fields = ('title',)
-    list_filter = ('date',)
+    list_filter = ('date', 'categories')
+    filter_horizontal = ('categories',)
+
+    def get_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all()])
+    get_categories.short_description = 'Categories'
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
-    list_filter = ('name',)
 
