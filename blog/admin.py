c6e86ebc4693eae89fc04ec9818493031b3b86a4
shagorrobidas <roysagor88@gmail.com>
2026-02-17 05:29:52 +0600

    modify admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from blog.models import Post, Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'get_categories', 'date')
-    search_fields = ('title',)
+    list_display = ('title', 'display_image', 'get_categories', 'date')
+    search_fields = ('title', 'summary', 'content')
     list_filter = ('date', 'categories')
     filter_horizontal = ('categories',)
+
+    def display_image(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="60" height="auto" style="border-radius: 5px;" />', obj.image.url)
+        return "No Image"
+    display_image.short_description = 'Preview'
 
     def get_categories(self, obj):
         return ", ".join([c.name for c in obj.categories.all()])
