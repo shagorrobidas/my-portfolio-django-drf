1059f3a8447d88a4fc3f0913fb34d6800caee319
shagorrobidas <roysagor88@gmail.com>
2026-02-17 05:34:11 +0600

    update admin

--- resume/admin.py
+++ resume/admin.py
@@ -4,22 +4,25 @@ from resume.models import Education, Experience, Skill
 
 @admin.register(Education)
 class EducationAdmin(admin.ModelAdmin):
-    list_display = ('degree', 'institution', 'start_date', 'end_date')
+    list_display = ('degree', 'institution', 'start_date', 'end_date', 'order')
+    list_editable = ('order',)
     list_filter = ('institution', 'degree')
-    search_fields = ('institution', 'degree', 'field_of_study')
-    ordering = ('-start_date',)
+    search_fields = ('institution', 'degree', 'description')
+    ordering = ('order', '-start_date')
 
 
 @admin.register(Experience)
 class ExperienceAdmin(admin.ModelAdmin):
-    list_display = ('company', 'start_date', 'end_date')
+    list_display = ('role', 'company', 'start_date', 'end_date', 'order')
+    list_editable = ('order',)
     list_filter = ('company',)
-    search_fields = ('company', 'description')
-    ordering = ('-start_date',)
+    search_fields = ('role', 'company', 'description')
+    ordering = ('order', '-start_date')
 
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('name', 'proficiency')
+    list_editable = ('proficiency',)
     search_fields = ('name',)
-    ordering = ('name',)
+    ordering = ('-proficiency', 'name')
