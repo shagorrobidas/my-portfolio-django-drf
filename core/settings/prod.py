91e70a3519d52f143f12e94fe4dd2e483f937eef
Shagor Robidas <roysagor88@gmail.com>
2026-05-21 23:56:06 +0600

    fix: base

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -25,23 +25,36 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://shagorrobidas.dev",
+    "https://shagorrobidas.dev",
     "http://www.shagorrobidas.dev",
+    "https://www.shagorrobidas.dev",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://shagorrobidas.dev",
-    "http://www.shagorrobidas.dev",
-    "https://shagorrobidas.dev",
-    "https://www.shagorrobidas.dev",
     "http://51.20.17.15",
     "https://51.20.17.15",
+    "http://shagorrobidas.dev",
+    "https://shagorrobidas.dev",
+    "http://www.shagorrobidas.dev",
+    "https://www.shagorrobidas.dev",
 ]
 
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# 🔥 IMPORTANT FIX (your issue was here)
+CSRF_COOKIE_SECURE = False   # set TRUE only when HTTPS fully working
+SESSION_COOKIE_SECURE = False # set TRUE only when HTTPS fully working
+
+CSRF_COOKIE_SAMESITE = "Lax"
+SESSION_COOKIE_SAMESITE = "Lax"
+
+# -------------------------
+# PROXY / SSL
+# -------------------------
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+# Enable only after SSL setup
+SECURE_SSL_REDIRECT = False
+
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
-
-SECURE_SSL_REDIRECT = False  # enable only after SSL setup
+X_FRAME_OPTIONS = 'SAMEORIGIN'
